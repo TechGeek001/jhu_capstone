9349505a2edb061fb7a5e832593c3e556c9ded35
Andrew Zitter <azitter@gmail.com>
2024-12-05 07:52:06 -0500

    Updated version number

--- drone_ips/__init__.py
+++ drone_ips/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for drone_ips."""
 
 # Import the Vehicle class from the vehicle module
-__version__ = "1.1.1"
+__version__ = "1.2.0"
 VERSION = tuple(int(part) for part in __version__.split("."))
