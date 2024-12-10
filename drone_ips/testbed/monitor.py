8d7ae43f1f3bf125652982725a5019ace76ac4a8
Andrew Zitter <azitter@gmail.com>
2024-12-10 08:11:39 -0500

    Manually added distance sensor value in testbed

--- drone_ips/testbed/monitor.py
+++ drone_ips/testbed/monitor.py
@@ -30,6 +30,9 @@ class Monitor(monitor.Monitor):
             The current data from the vehicle.
         """
         current_data = super().get_vehicle_data()
+        # Manually add the distance sensor value since the default SITL drone doesn't have it
+        if "rangefinder.distance" not in current_data:
+            current_data["rangefinder.distance"] = max(0, int(current_data["location.relative_frame.alt"] * 100)) + 5
         # This is where simulated attacks are injected
         current_data.update(self.attack_manager.attack(current_data, self.last_data))
         return current_data
